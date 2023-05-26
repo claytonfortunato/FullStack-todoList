706506a97330c594c6599a1daa575ce5bafebe28
claytonfortunato <claytonreis10@gmail.com>
2023-05-25 23:37:05 -0300

    feat: server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express();
+
+app.get("/", () => {
+  return "hello world";
+});
+
+const port = 8080;
+
+app.listen(port, () => console.log(`Rodando com express na porta ${port}!`));
